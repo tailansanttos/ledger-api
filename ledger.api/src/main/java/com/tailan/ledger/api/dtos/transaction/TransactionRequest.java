b6cfa22edfbab45911803e2a82f5fd6fa30df013
tailan <tailansanttos02@gmail.com>
2025-09-18 23:44:31 -0300

    feat: adicionado um novo campo que vai ser um identificador passado pelo cliente para evitar duplicação de transações.

--- ledger.api/src/main/java/com/tailan/ledger/api/dtos/transaction/TransactionRequest.java
+++ ledger.api/src/main/java/com/tailan/ledger/api/dtos/transaction/TransactionRequest.java
@@ -2,5 +2,5 @@ package com.tailan.ledger.api.dtos.transaction;
 
 import java.math.BigDecimal;
 
-public record TransactionRequest(BigDecimal value) {
+public record TransactionRequest(BigDecimal value, String externalId) {
 }
