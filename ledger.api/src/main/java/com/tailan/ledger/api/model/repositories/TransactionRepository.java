90d24d9addac180186d61acbac4aab38ef5d9be6
tailan <tailansanttos02@gmail.com>
2025-09-18 23:03:35 -0300

    feat: repositorios criado.

---
+++ ledger.api/src/main/java/com/tailan/ledger/api/model/repositories/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.tailan.ledger.api.model.repositories;
+
+import com.tailan.ledger.api.model.domain.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
