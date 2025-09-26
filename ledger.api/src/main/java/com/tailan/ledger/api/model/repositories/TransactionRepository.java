4e814631119428644b423fbd1f3ac635f750e6eb
tailan <tailansanttos02@gmail.com>
2025-09-26 09:48:08 -0300

    feat: adicionado metodo para o usuario pegar o extrato da sua conta, retornando com paginação.

--- ledger.api/src/main/java/com/tailan/ledger/api/model/repositories/TransactionRepository.java
+++ ledger.api/src/main/java/com/tailan/ledger/api/model/repositories/TransactionRepository.java
@@ -1,6 +1,9 @@
 package com.tailan.ledger.api.model.repositories;
 
+import com.tailan.ledger.api.model.domain.Account;
 import com.tailan.ledger.api.model.domain.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +13,7 @@ import java.util.UUID;
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
     boolean existsByExternalId(String externalId);
     Transaction findByExternalId(String externalId);
+
+    Page<Transaction> findAllByAccount(Account account, Pageable pageable);
+
 }
--- ledger.api/src/main/java/com/tailan/ledger/api/service/TransactionService.java
+++ ledger.api/src/main/java/com/tailan/ledger/api/service/TransactionService.java
@@ -2,10 +2,15 @@ package com.tailan.ledger.api.service;
 
 import com.tailan.ledger.api.dtos.transaction.TransactionRequest;
 import com.tailan.ledger.api.dtos.transaction.TransactionResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
 
 public interface TransactionService {
     public TransactionResponse depositAccount(UUID accountId, TransactionRequest transactionRequest);
     public TransactionResponse accountWithdrawal(UUID accountId, TransactionRequest transactionRequest);
+
+    public Page<TransactionResponse> getExtract(UUID accountId, int page, int size);
 }
